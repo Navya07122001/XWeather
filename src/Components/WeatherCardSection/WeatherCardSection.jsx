8a6ce1aadd5763eb4d01e7e54b65a1db245e8546
Navya07122001 <navyasreeparvatham@gmail.com>
2024-08-30 17:32:35 +0530

    changed file name

--- src/Components/WeatherCardSection/WeatherCardSection.jsx
+++ src/Components/WeatherCardSection/WeatherCardSection.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import styles from './WeatherCardSection.module.css';
+import './WeatherCardSection.css'
 
 const WeatherCardSection = ({ xdata, xdata1 }) => {
   return (
-    <div className={styles['weather-cards']}>  
-      <div className={styles['weather-card']}>{xdata1}</div> 
+    <div className="weather-cards">  {/* Removed the leading dot */}
+      <div className="weather-card">{xdata1}</div>  
       <div style={{ fontSize: '1.5rem' }}>{xdata}</div>
     </div>
   );
