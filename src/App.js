e1ec23515b0bed7129296278f46917ba9b83c658
Navya07122001 <navyasreeparvatham@gmail.com>
2024-08-30 16:51:10 +0530

    weatherapp implemented

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WeatherSection from './Components/WeatherSection';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherSection/>
+    </div>
+  );
+}
+
+export default App;
